20e143c1fd907bf56923832ec9b9e89de6ef7465
Jelson Alfarero <heyjelson@gmail.com>
2023-03-27 23:04:38 +0800

    create functions that disabled buttons and reset default and dynamic features

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
+//access the HTML element
 const grid = document.querySelector('.grid_container');
-const button = document.querySelector('.btn');
+const defaultBtn = document.querySelector('.default');
+const customBtn = document.querySelector('.custom');
+const resetBtn = document.querySelector('.reset');
 
+//disable buttons
+function disableBtn() {
+    defaultBtn.disabled = true;
+    customBtn.disabled = true;
+}
+
+//default square grid box
 function defaultGrid() {
-    let defaultGridBox = 400 / 16 - 2 + 'px';
+    let gridBox = 400 / 16 - 2 + 'px';
     for(let i = 0; i < 256; i++) {
         const inner_grid = document.createElement('div');
-        inner_grid.style.cssText = `display: inline-block; height: ${defaultGridBox}; width: ${defaultGridBox}; border: 1px solid #0174a1`;
+        inner_grid.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
         grid.appendChild(inner_grid);
     }
+    disableBtn();
 }
 
+//prompt, asking number to creates square grid box
 function popUp() {
     let number = prompt("Enter number !> 100 to creates new grid square");
-    let enteredNumber = button.value = number;
-    let gridBox = 400 / enteredNumber - 2 + 'px'; 
-    console.log(gridBox);
+    let enteredNumber = customBtn.value = number;
+    gridBox = 400 / enteredNumber - 2 + 'px'; 
     for(let i = 0; i < enteredNumber * enteredNumber; i++) {
         const inner_grid = document.createElement('div');
-        inner_grid.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1; z-index: 10`;
+        inner_grid.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
         grid.appendChild(inner_grid);
     }
+    disableBtn();
 }
+
+//reset functions
+function resetGrid() {
+    window.location.reload();
+}
+
+//events
+defaultBtn.addEventListener('click', defaultGrid)
+customBtn.addEventListener('click', popUp);
+resetBtn.addEventListener('click', resetGrid);
+
+
+
 
