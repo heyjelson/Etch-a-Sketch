b71d0c7dd76b1ef452144c0e2b06ef9b99287d84
Jelson Alfarero <heyjelson@gmail.com>
2023-03-30 01:36:32 +0800

    all done by completing the hover effects and coditional statements. Project is finished!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//access the HTML element
+//access the HTML elements
 const grid = document.querySelector('.grid_container');
 const defaultBtn = document.querySelector('.default');
 const customBtn = document.querySelector('.custom');
@@ -11,39 +11,51 @@ function disableBtn() {
 }
 
 //default square grid box
+let innerDefault;
 function defaultGrid() {
     let gridBox = 400 / 16 - 2 + 'px';
     for(let i = 0; i < 256; i++) {
-        const inner_grid = document.createElement('div');
-        inner_grid.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
-        grid.appendChild(inner_grid);
+        innerDefault = document.createElement('div');
+        innerDefault.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
+        innerDefault.addEventListener('mouseover', function() {this.style.background = 'black'});
+        grid.appendChild(innerDefault);
     }
+    
     disableBtn();
 }
 
+defaultBtn.addEventListener('click', defaultGrid);
+
+
+
 //prompt, asking number to creates square grid box
+let number;
+let enteredNumber;
+let innerCustom;
 function popUp() {
-    let number = prompt("Enter number !> 100 to creates new grid square");
-    let enteredNumber = customBtn.value = number;
-    gridBox = 400 / enteredNumber - 2 + 'px'; 
-    for(let i = 0; i < enteredNumber * enteredNumber; i++) {
-        const inner_grid = document.createElement('div');
-        inner_grid.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
-        grid.appendChild(inner_grid);
+    number = prompt("Enter number !> 100 to creates new grid square");
+    enteredNumber = customBtn.value = number;
+    if(enteredNumber > 100) {
+        alert("Please enter number 100 or less than 100. Click ok then reset to continue.");
+        enteredNumber = customBtn.value = '';
     }
-    disableBtn();
+    gridBox = 400 / enteredNumber - 2 + 'px'; 
+    let rgb = ['red', 'green', 'blue'];
+    for(let i = 0; i < enteredNumber * enteredNumber; i++) {
+        innerCustom = document.createElement('div');
+        innerCustom.style.cssText = `display: inline-block; height: ${gridBox}; width: ${gridBox}; border: 1px solid #0174a1`;
+        innerCustom.addEventListener('mouseover', function() {this.style.background = rgb[Math.floor(Math.random() * rgb.length)]});
+        grid.appendChild(innerCustom);
+    }
+    disableBtn();   
 }
+
+customBtn.addEventListener('click', popUp);
+
 
 //reset functions
 function resetGrid() {
     window.location.reload();
 }
 
-//events
-defaultBtn.addEventListener('click', defaultGrid)
-customBtn.addEventListener('click', popUp);
 resetBtn.addEventListener('click', resetGrid);
-
-
-
-
